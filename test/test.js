b3a1889fb7c4aacac0e28d988e5432232448c06c
George Moon <george.moon@gmail.com>
2021-11-01 12:42:10 -0400

    added unit test

---
+++ test/test.js
@@ -0,0 +1,19 @@
+const test = require('ava');
+const app = require('../express-s3proxy.js');
+const http = require('http');
+const s3proxy = require('s3proxy');
+const request = require('supertest');
+
+test.before(async t => {
+	t.context.server = http.createServer(app);
+});
+
+test.after.always(t => {
+	t.context.server.close();
+});
+
+test('get /health', async t => {
+	const response = await request(t.context.server).get('/health');
+	t.is(response.statusCode, 200);
+});
+
