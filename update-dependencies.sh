aece4dbb8cdba285be417650f2e9429a6f6964b9
George Moon <george.moon@gmail.com>
2021-11-06 19:20:11 -0400

    implement linter suggestions

--- update-dependencies.sh
+++ update-dependencies.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 echo Pulling node:current-alpine
-export DIGEST=$(docker pull node:current-alpine | grep Digest | cut -d ' ' -f 2)
+DIGEST=$(docker pull node:current-alpine | grep Digest | cut -d ' ' -f 2)
+export DIGEST
 echo "DIGEST=${DIGEST}"
 
-echo Updating Dockerfile with Digest ${DIGEST}
+echo Updating Dockerfile with Digest "${DIGEST}"
 cp Dockerfile Dockerfile.bak
 perl -pe 's/^FROM node:current-alpine.*$/FROM node:current-alpine\@$ENV{DIGEST} as base/' Dockerfile.bak >Dockerfile
 rm Dockerfile.bak
